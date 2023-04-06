00ae0fc7dbdd6f470a0ecf0402a360d0aa7ddc94
Yuxuan Wang <wang7208801@126.com>
2023-04-06 17:25:02 +0800

    Update CCGExample.py

--- CCGExample.py
+++ CCGExample.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Apr  2 17:21:33 2023
-
 @author: wyx
 """
 from ExampleMatrix import Ay, by, G, E, M, h, bias
@@ -34,6 +33,7 @@ MP.optimize()
 MP_obj = MP.ObjVal
 LB = max(MP_obj, LB)
 
+
 SP = Model('SP')
 x = SP.addMVar((9,),vtype=GRB.CONTINUOUS,name='x')
 pi = SP.addMVar(G.shape[0],vtype=GRB.CONTINUOUS,name='pi')
@@ -53,6 +53,7 @@ SP.setObjective(b@x,GRB.MAXIMIZE)
 SP.optimize()
 SP_obj = SP.ObjVal    
 UB = min(UB, c@y.x+a@z.x+SP_obj)
+MP.reset()
 while abs(UB-LB)>=epsilon:
     # add x^{k+1}
     x_new = MP.addMVar((9,), vtype=GRB.CONTINUOUS)
@@ -60,22 +61,24 @@ while abs(UB-LB)>=epsilon:
     MP.addConstr(eta>=b.T@x_new)
     # Ey+Gx^{k+1}>=h-Mu_{k+1}
     MP.addConstr(E[:,:3]@y+E[:,3:]@z+G@x_new>=h-M@g.x)
+    SP.reset()
     MP.optimize()
     LB = max(LB, MP.objval)
+    
     # update the SP constrs according to the MP solution
-    SP.remove(G1)
-    SP.remove(G2)
+    SP.remove(G1[:3])
+    SP.remove(G2[:3])
     G1 = SP.addConstr(G@x>=h-M@g-E@np.concatenate([y.x,z.x]),name="G1")
     G2 = SP.addConstr(G@x-h+E@np.concatenate([y.x,z.x])+M@g<=bigM*(1-v),name='G2')
+    
     SP.optimize()
+    
     # obtain the optimal y^{k+1}
     SP_obj = SP.ObjVal
     UB = min(UB, c@y.x+a@z.x+SP_obj)
-    
+    MP.reset()
     k += 1
     # go back to the MP
     print("经过{}次迭代".format(k))
     print("上界为：{}".format(UB))
     print("下界为：{}".format(LB))
-
-    
